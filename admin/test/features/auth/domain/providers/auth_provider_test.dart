7c0c8a5f716dfeefb12832ee5e7311e21018bcc9
Steven Gunanto <>
2023-11-23 12:07:36 +0700

    add auth_provider_test

---
+++ admin/test/features/auth/domain/providers/auth_provider_test.dart
@@ -0,0 +1,40 @@
+import 'package:admin/features/auth/data/datasource/auth_data_source.dart';
+import 'package:admin/features/auth/domain/providers/auth_provider.dart';
+import 'package:admin/features/auth/domain/repositories/auth_repository.dart';
+import 'package:admin/shared/domain/providers/supabase_service_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../mocks.dart';
+
+void main() {
+  final mockSupabaseService = MockSupabaseService();
+  final mockSupabaseAuthService = MockSupabaseAuthService();
+  final providerContainer = ProviderContainer(overrides: [
+    supabaseServiceProvider.overrideWithValue(mockSupabaseService),
+    supabaseAuthServiceProvider.overrideWithValue(mockSupabaseAuthService)
+  ]);
+  late dynamic authDataSource;
+  late dynamic authRepository;
+
+  setUpAll(
+    () {
+      authDataSource = providerContainer
+          .read(authDataSourceProvider(supabaseClient: mockSupabaseService));
+      authRepository = providerContainer.read(authRepositoryProvider);
+    },
+  );
+
+  test('dataSourceProvider is a AuthDataSource', () {
+    expect(
+      authDataSource,
+      isA<AuthDataSource>(),
+    );
+  });
+  test('authRepositoryProvider is AuthRepository', () {
+    expect(
+      authRepository,
+      isA<AuthRepository>(),
+    );
+  });
+}
