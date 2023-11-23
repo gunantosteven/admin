5163a9f984cc435d1ac9974290835dcd0e8911de
Steven Gunanto <>
2023-11-23 10:44:53 +0700

    add auth_controller_test

---
+++ admin/test/features/auth/application/auth_controller_test.dart
@@ -0,0 +1,96 @@
+import 'package:admin/features/auth/application/auth_controller.dart';
+import 'package:admin/features/auth/domain/providers/auth_provider.dart';
+import 'package:admin/features/auth/domain/repositories/auth_repository.dart';
+import 'package:admin/shared/exception/http_exception.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../dummy_data.dart';
+import '../../../mocks.dart';
+
+void main() {
+  const testEmail = 'test@email.com';
+
+  ProviderContainer makeProviderContainer(AuthRepository authRepository) {
+    final container = ProviderContainer(
+      overrides: [
+        authRepositoryProvider.overrideWithValue(authRepository),
+      ],
+    );
+    addTearDown(container.dispose);
+    return container;
+  }
+
+  setUpAll(() {
+    registerFallbackValue(const AsyncLoading<bool>());
+  });
+
+  group('signInWithOtp', () {
+    test('success', () async {
+      // setup
+      final authRepository = MockAuthRepository();
+      when(() => authRepository.signInWithOtp(email: testEmail)).thenAnswer(
+        (_) async => const Right<AppException, bool>(
+          true,
+        ),
+      );
+      final container = makeProviderContainer(authRepository);
+      final listener = Listener<AsyncValue<bool>>();
+      container.listen(
+        authControllerProvider,
+        listener.call,
+        fireImmediately: true,
+      );
+      const initialValue = AsyncData<bool>(false);
+      // verify initial value from build method
+      verify(() => listener(null, initialValue));
+      final controller = container.read(authControllerProvider.notifier);
+
+      // run
+      await controller.signInWithOtp(email: testEmail);
+
+      // verify
+      verifyInOrder([
+        // transition from data to loading state
+        () => listener(initialValue, any(that: isA<AsyncLoading>())),
+        // transition from loading state to data
+        () => listener(
+            any(that: isA<AsyncLoading>()), const AsyncData<bool>(true)),
+      ]);
+      verifyNoMoreInteractions(listener);
+    });
+
+    test('failure', () async {
+      // setup
+      final authRepository = MockAuthRepository();
+      when(() => authRepository.signInWithOtp(email: testEmail))
+          .thenAnswer((_) async => Left<AppException, bool>(ktestAppException));
+      final container = makeProviderContainer(authRepository);
+      final listener = Listener<AsyncValue<bool>>();
+      container.listen(
+        authControllerProvider,
+        listener.call,
+        fireImmediately: true,
+      );
+      const data = AsyncData<bool>(false);
+      // verify initial value from build method
+      verify(() => listener(null, data));
+      final controller = container.read(authControllerProvider.notifier);
+
+      // run
+      await controller.signInWithOtp(email: testEmail);
+
+      // verify
+      verifyInOrder([
+        // set loading state
+        () => listener(data, any(that: isA<AsyncLoading>())),
+        // error when complete
+        () => listener(
+            any(that: isA<AsyncLoading>()), any(that: isA<AsyncError>())),
+      ]);
+      verifyNoMoreInteractions(listener);
+    });
+  });
+}
--- admin/test/mocks.dart
+++ admin/test/mocks.dart
@@ -1,6 +1,13 @@
+import 'package:admin/features/auth/domain/repositories/auth_repository.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class MockSupabaseService extends Mock implements SupabaseClient {}
 
 class MockSupabaseAuthService extends Mock implements GoTrueClient {}
+
+class MockAuthRepository extends Mock implements AuthRepository {}
+
+class Listener<T> extends Mock {
+  void call(T? previous, T next);
+}
